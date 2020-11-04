3f1cbbd14eeab8e365e3f01bf29b9fbbf6b347c7
Gako358 <gako358@outlook.com>
2020-11-04 20:53:20 +0100

    volume added

--- config.def.h
+++ config.def.h
@@ -69,6 +69,7 @@ static const struct arg args[] = {
     { ram_used,     "<\x05 %s",        NULL            },
     { cpu_freq,     "|\x05 %4sHz",     NULL            },
 	{ datetime,     "<\x01 %s",         "%a, %d %b %T"  },
+    { run_command,  "<\x07 %s",        "amixer sget Master | awk -F\"[][]\" '/%/ { print $2  }' | head -n1"    },
     { keymap,       "<\x02%s",          NULL            },
     //{ battery_perc, "<\x03  %s",       "BAT0"          },
 };
